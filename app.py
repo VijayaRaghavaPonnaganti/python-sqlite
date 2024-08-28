b46b0210ee286aeef25542f8149cb576e90a7cce
VijayaRaghavaPonnaganti <vponnaga@kent.edu>
2024-08-27 21:25:08 -0400

    Update and rename app.py. to app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+#connect to SQLite Database
+conn = sqlite3.connect('example.db')
+cursor = conn.cursor()
+
+#create a table
+cursor.execute('''CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)''')
+
+#Insert a record 
+cursor.execute("INSERT INTO user (name, age) VALUES (?, ?)", ('Alice', 30))
+
+#Commit the changes
+conn.commit()
+
+#Query the database
+cursor.execute("SELECT * FROM user")
+rows = cursor.fetchall()
+
+for row in rows:
+  print(row)
+
+#close the connection
+conn.close()
+

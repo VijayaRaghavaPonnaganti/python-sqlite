3c7ce5a9779891b8c92f121069615584968a70ad
VijayaRaghavaPonnaganti <vponnaga@kent.edu>
2024-09-05 04:22:16 +0000

    Initial setup with Python and SQLITE example

--- task1.py
+++ task1.py
@@ -1,1 +1,27 @@
+import sqlite3
+
+# Connect to an SQLite database (or create it if it doesn't exist)
+connect2 = sqlite3.connect('new.db')
+
+# Create a cursor object using the cursor() method
+cursor = connect2.cursor()
+
+# Create table
+cursor.execute('''CREATE TABLE IF NOT EXISTS stk1
+             (date text, trans text, symbol text, qty real, price real)''')
+
+# Insert a row of data
+cursor.execute("INSERT INTO stk1 VALUES ('2024-01-05','SEL', 'A', 88, 25.12)")
+
+# Save (commit) the changes
+connect2.commit()
+
+cursor.execute("SELECT * FROM stk1")
+rows = cursor.fetchall()
+
+for row in rows:
+  print(row)
+
+# Close the connection
+connect2.close()
 
